0c88fa8584996e601e0ee392ae7ccde96d9c2c07
HuangJianMei <fz-c@qq.com>
2022-07-16 16:57:36 +0800

    学习rabbitmq 2022/7/16 提交

---
+++ src/main/java/com/cfz/learnrabbitmq/LearnRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.cfz.learnrabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnRabbitmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnRabbitmqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cfz/learnrabbitmq/LearnRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cfz.learnrabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnRabbitmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
